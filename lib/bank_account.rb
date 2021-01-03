9ef43a7c68ef3a74a9583eef0f3d34e69d28ce3c
Coby <cobyrpilpel2@gmail.com>
2021-01-03 15:58:52 -0500

    commit tree c8649db951900659b68b4b304f2a969b513d1808

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
+attr_accessor :balance, :status
+attr_reader :name
+@@all = []
+
+
+def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance
+    @status = status
+    @@all << self
+end
+def self.all
+    @@all
+end
+def deposit(money)
+    self.balance += money
+end
+def display_balance
+    "Your balance is $#{self.balance}."
+end
+def valid?
+    if @status == "open" && @balance > 0
+        true
+    else
+        false
+    end
+end
+def close_account
+    @status = "closed"
+end
+
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,51 @@
 class Transfer
-  # your code here
+attr_accessor :amount
+attr_reader :sender, :receiver, :status
+@@all = []
+
+def initialize(sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @status = "pending"
+  @amount = amount
+  @@all << self
+end
+def valid?
+  if self.amount < self.sender.balance
+  self.sender.valid?
+  self.receiver.valid?
+  end
+end
+def execute_transaction
+  if self.valid? && @status == "pending" && @sender.balance > @amount
+  @sender.balance -= @amount
+  @receiver.balance += @amount
+  @status = "complete"
+  else
+  @status = "rejected"
+  "Transaction rejected. Please check your account balance."
+  end
+end
+
+#def reverse_transfer        First attempt. Not sure why this failed "Can reverse transfer between two accounts test"
+  #if self.status == "complete"
+  #self.sender.balance += amount
+  #self.receiver.balance -= amount
+  #self.status = "reversed"
+  #end
+#end
+
+def reverse_transfer
+  if self.execute_transaction
+    @sender.balance += @amount
+    @receiver.balance -= @amount
+    @status = "reversed"
+  end
+end
+
+
+
+
+
+
 end
